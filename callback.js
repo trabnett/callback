d6ac23da962375736461857e1170b05c6179d6ce
trabnett <timrabnett@gmail.com>
2019-01-10 17:35:40 +0000

    callback refactored with forEach

--- callback.js
+++ callback.js
@@ -1,9 +1,9 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(item,index) {
+    if (item === 'Waldo') {
+      found(index)
     }
-  }
+  })
 }
 
 function actionWhenFound(index) {
@@ -11,3 +11,6 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
